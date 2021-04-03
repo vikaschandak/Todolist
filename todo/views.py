5267fbae94a9b8b29a5325dc851ba3ca8c1713f6
Vikas <vikaschandak05@gmail.com>
2021-04-04 03:46:38 +0530

    added model

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 def index(request):
-    return render(request, 'index.html')
+    todo = Todo.objects.all()
+    if request.method=='POST':
+        new_todo = Todo(
+            title=request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+
+    return render(request, 'index.html',{'todos':todo})
+
+
+def delete(request, pk):
+    todo = Todo.objects.get(id = pk)
+    todo.delete()
+    return redirect('/')
